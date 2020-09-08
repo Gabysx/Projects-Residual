976dd6196ff03e3aeab99585076455778f72e6cd
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2020-09-08 19:19:39 -0300

    conexão firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,11 @@
+firebaseConfig = {
+    apiKey: "AIzaSyCtxKy6KRtN5a81aox7QGkIPFilNBluKGE",
+    authDomain: "residual-6b31a.firebaseapp.com",
+    databaseURL: "https://residual-6b31a.firebaseio.com",
+    projectId: "residual-6b31a",
+    storageBucket: "residual-6b31a.appspot.com",
+    messagingSenderId: "179433488727",
+    appId: "1:179433488727:web:948223fa6580ee61e67e12"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
